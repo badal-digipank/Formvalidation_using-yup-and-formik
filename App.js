5551f47a712bf3f978eb8c7370b674e51878afa3
badal-digipank <81536764+badal-digipank@users.noreply.github.com>
2021-04-23 08:51:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ValidatedLoginForm from './ValidatedLoginForm';
+import "./App.css";
+function App(){
+  return(
+    <div> 
+      <ValidatedLoginForm/>
+    </div>
+  );
+}
+
+export default App
